25e24832ecd6c56f46aea7a8f817c1f1ba048275
DewArchLaptop <kodamcmann@yahoo.com>
2026-04-08 21:42:08 -0500

    Started Etch a Sketch project

---
+++ Projects/EtchASketch/script.js
@@ -0,0 +1,40 @@
+//------------------------------------------
+//-------------- DOM OBJECTS ---------------
+//------------------------------------------
+
+const gameContainer = document.querySelector("#GameContainer")
+
+//------------------------------------------
+//------------- DOM FUNCTIONS --------------
+//------------------------------------------
+
+//------------------------------------------
+//--------------- FUNCTIONS ----------------
+//------------------------------------------
+
+// Will always make a sqaure grid and functions expects int as argument
+// If function receives 5 as the argument, will create grid 5x5
+function createBoard(gridSize){
+
+	for(let i = 0; i < gridSize; ++i){
+		const rowDiv = document.createElement("div")
+		rowDiv.classList.add("gridRow")
+
+		for(let j = 0; j < gridSize; ++j){
+			const columnDiv = document.createElement("div")
+			columnDiv.classList.add("gridColumn")
+			columnDiv.textContent = "."
+			rowDiv.appendChild(columnDiv)
+		}
+
+		gameContainer.appendChild(rowDiv);
+	}
+			
+}
+
+
+//------------------------------------------
+//----------------- MAIN -------------------
+//------------------------------------------
+
+createBoard(5)
