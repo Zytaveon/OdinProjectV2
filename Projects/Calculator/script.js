852242a83b67c922257a57e61175b3854c10ccba
Dew <kodamcmann@yahoo.com>
2026-04-12 21:55:15 -0500

    Started created Eval function and trying to display to screen. Realized I need to change the variables to effect the global variables so can use functions instead of same 3 lines in every listener

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -2,6 +2,8 @@
 //--------------- DOM OBJECTS -------------------
 //-----------------------------------------------
 
+const calcDisplay = document.querySelector("#Display")
+
 //------------- Utility Buttons -----------------
 
 const backButton = document.querySelector("#Back_Button")
@@ -52,78 +54,151 @@ percentButton.addEventListener("click", ()=>{
 //------------- Numbers Buttons -----------------
 
 zeroButton.addEventListener("click", ()=> {
-  console.log("Zero")
+  if(operationPlaced){firstNumber = numberPressed("0",firstNumber)}
+  else{secondNumber = numberPressed("0", secondNumber)}
 })
 
 oneButton.addEventListener("click", ()=> {
-  console.log("One")
+  if(operationPlaced){firstNumber = numberPressed("1",firstNumber)}
+  else{secondNumber = numberPressed("1", secondNumber)}
 })
 
 twoButton.addEventListener("click", ()=> {
-  console.log("Two")
+  if(operationPlaced){firstNumber = numberPressed("2",firstNumber)}
+  else{secondNumber = numberPressed("2", secondNumber)}
 })
 
 threeButton.addEventListener("click", ()=> {
-  console.log("Three")
+  if(operationPlaced){firstNumber = numberPressed("3",firstNumber)}
+  else{secondNumber = numberPressed("3", secondNumber)}
 })
 
 fourButton.addEventListener("click", ()=> {
-  console.log("Four")
+  if(operationPlaced){firstNumber = numberPressed("4",firstNumber)}
+  else{secondNumber = numberPressed("4", secondNumber)}
 })
 
 fiveButton.addEventListener("click", ()=> {
-  console.log("Five")
+  if(operationPlaced){firstNumber = numberPressed("5",firstNumber)}
+  else{secondNumber = numberPressed("5", secondNumber)}
 })
 
 sixButton.addEventListener("click", ()=> {
-  console.log("Six")
+  if(operationPlaced){firstNumber = numberPressed("6",firstNumber)}
+  else{secondNumber = numberPressed("6", secondNumber)}
 })
 
 sevenButton.addEventListener("click", ()=> {
-  console.log("Seven")
+  if(operationPlaced){firstNumber = numberPressed("7",firstNumber)}
+  else{secondNumber = numberPressed("7", secondNumber)}
 })
 
 eightButton.addEventListener("click", ()=> {
-  console.log("Eight")
+  if(operationPlaced){firstNumber = numberPressed("8",firstNumber)}
+  else{secondNumber = numberPressed("8", secondNumber)}
 })
 
 nineButton.addEventListener("click", ()=> {
-  console.log("Nine")
+  if(!operationPlaced){firstNumber = numberPressed("9",firstNumber)}
+  else{secondNumber = numberPressed("9", secondNumber)}
+
+  addCharacterToDisplay("9")
+  calcDisplay.innerText = display
 })
 
 
 signButton.addEventListener("click", ()=> {
-  console.log("Sign")
+  console.log("Sign Button, I don't do anything yet :(")
 })
 
 decimalButton.addEventListener("click", ()=>{
-  console.log("Decimal")
+  console.log("Decimal Button, I don't do anything yet :(")
 })
 
 //------------ Operation Buttons ----------------
 
 divideButton.addEventListener("click", ()=>{
-  console.log("Divide")
+  operation = "/"
+  operationPlaced = true;
+  addCharacterToDisplay(" / ")
+
 })
 
+
 multiplyButton.addEventListener("click", ()=>{
-  console.log("Multiply")
+  operation = "*"
+  operationPlaced = true;
 })
 
 minusButton.addEventListener("click", ()=>{
-  console.log("Minus")
+  operation = "-"
+  operationPlaced = true;
 })
 
 addButton.addEventListener("click", ()=>{
-  console.log("Add")
+  operation = "+"
+  operationPlaced = true;
 })
 
 equalButton.addEventListener("click", ()=>{
-  console.log("Equal")
+  evaulate(firstNumber, secondNumber, operation)
 })
+
+//-----------------------------------------------
+//---------------- Functions --------------------
+//-----------------------------------------------
+
+//The argument should come in a string
+function numberPressed(numberPressed, currentNumber){
+  return currentNumber + numberPressed
+}
+
+function addCharacterToDisplay(char){
+  return display = display + char;
+}
+
+//The arguments will come as strings
+function evaulate(number1, number2, operation){
+
+  let num1 = parseInt(number1)
+  let num2 = parseInt(number2)
+
+  let result = 0;
+
+  switch(operation){
+
+    case "+":
+      result = num1 + num2;
+      break;
+
+    case '-':
+      result = num1 - num2;
+      break;
+
+    case "*":
+      result = num1 * num2;
+      break;
+
+    case "/":
+      result = num1 / num2;
+      break;
+  }
+  
+  console.log(result)
+  return result
+
+}
 
 //-----------------------------------------------
 //------------------ MAIN -----------------------
 //-----------------------------------------------
 
-console.log("Hello!")
+let firstNumber = ""
+let secondNumber = ""
+let operation
+let operationPlaced = false;
+
+let display = ""
+
+console.log(calcDisplay.innerText)
+
