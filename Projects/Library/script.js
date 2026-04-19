1a210e9177e722bbeb8b47b1743ee0c83ed21edd
DewArchLaptop <kodamcmann@yahoo.com>
2026-04-19 15:34:31 -0500

    Added basic testing styles to css. Started building the library. Currently creates dummy books and adds them to the DOM and they are displayed.

--- Projects/Library/script.js
+++ Projects/Library/script.js
@@ -1,5 +1,54 @@
-const HelloButton = document.querySelector("#HelloButton")
+//---------------------------------------------
+//--------------- DOM OBJECTS -----------------
+//---------------------------------------------
 
-HelloButton.addEventListener("click", ()=> {
-    console.log("hello from the button")
+const BookContainer = document.querySelector("#BookContainer")
+const AddBookButton = document.querySelector("#AddBookButton")
+
+//---------------------------------------------
+//-------------- DOM FUNCTIONS ----------------
+//---------------------------------------------
+
+AddBookButton.addEventListener("click", ()=> {
+    console.log("Hello from Add Button!")
 })
+
+//---------------------------------------------
+//------------------ MAIN? --------------------
+//---------------------------------------------
+
+const myLibrary = [];
+
+function Book(title, author, pages, read, id){
+
+    if(!new.target){
+        throw Error("You must use the 'new' operator to call the constructor")
+    }
+
+    this.title = "Blank"
+    this.author = "Blank"
+    this.pages = 0
+    this.read = false
+    this.id = "Blank"
+}
+
+//Should already have the book created?
+function addBookToLibrary(){
+    
+}
+
+function displayBooks(){
+    for(let i = 0;  i < myLibrary.length; ++i){
+        const BookDiv = document.createElement("div");
+        BookDiv.classList.add("Book")
+        BookContainer.appendChild(BookDiv)
+    }
+}
+
+const Book1 = new Book("Dew", "Dew", 100, false, 111)
+const Book2 = new Book("Dewart", "Dewart", 111, true, 222)
+
+myLibrary.push(Book1)
+myLibrary.push(Book2)
+
+displayBooks()
