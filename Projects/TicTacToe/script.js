075564182fb8fdc81b316fb4f18df62ee3170283
DewArchLaptop <kodamcmann@yahoo.com>
2026-04-27 13:18:12 -0500

    Added form to get players names. Trying to get players into the Game IFEE. Currently having issues trying to call function out of Player objects

--- Projects/TicTacToe/script.js
+++ Projects/TicTacToe/script.js
@@ -8,7 +8,11 @@
 //---------------- DOM OBJECTS -------------------
 //------------------------------------------------
 
-
+const startButton       = document.querySelector("#StartButton")
+const PlayerDialog      = document.querySelector("#PlayerNameDialog")
+const PlayerNameForm    = document.querySelector("#PlayerNameForm")
+const FormPlayerOne     = document.querySelector("#FormPlayerOne")
+const FormPlayerTwo     = document.querySelector("#FormPlayerTwo")
 
 //------------------------------------------------
 //------------------ OBJECTS ---------------------
@@ -28,7 +32,7 @@ const GameBoard = (() => {
                 gameBoard[i][j] = null
             }
         }
-    };
+    }
 
     function printBoard(){
         let output = "GameBoard: \n"
@@ -38,20 +42,45 @@ const GameBoard = (() => {
             }
             output += "\n"
         }
-
-        console.log(output)
-    };
+    }
+    
+    function setTile(i, j, value){
+        if(gameBoard[i][j] === null){
+            gameBoard[i][j] = value
+        }
+        else{
+            console.log("Set tile did nothing")
+        }
+    }
 
     //Return the functions so they can be used
     return{
         createBoard,
-        printBoard
+        printBoard,
+        setTile
     }
 
 })();
 
 const Game = (() => {
-    
+
+    let playerOne
+    let playerTwo
+
+    //Should be receiving player objects already
+    function setPlayers(PlayerOne, PlayerTwo){
+        playerOne = PlayerOne
+        playerTwo = PlayerTwo
+
+        console.log("Hello, " + playerOne.getPlayerName())
+        console.log("Hello, " + playerTwo.getPlayerName())
+
+    }
+
+    return{
+        setPlayers
+    }
+
 })();
 
 
@@ -59,13 +88,31 @@ const Game = (() => {
 //--------------- DOM FUNCTIONS ------------------
 //------------------------------------------------
 
+startButton.addEventListener("click", () => {
+    console.log("Hello from the start button")
+    PlayerDialog.showModal()
+})
 
+PlayerNameForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    Game.setPlayers(Player(FormPlayerOne.value, "X"), Player(FormPlayerTwo.value, "O"))
+    PlayerDialog.close()
+})
 
 //------------------------------------------------
 //----------------- FUNCTIONS --------------------
 //------------------------------------------------
 
 function Player(username, userSymbol){
+
+    console.log("Hello, " + username + ". Your symbol is: " + userSymbol)
+
+    let name = username
+    let symbol = userSymbol
+
+    function getPlayerName(){
+        return name
+    }
 }
 
 
